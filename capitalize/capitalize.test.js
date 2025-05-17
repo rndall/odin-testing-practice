ed01de3b6654caef083d3f2a5092d9e5d05233b4
Ran <96964503+rndall@users.noreply.github.com>
2025-05-17 23:52:18 +0800

    test(capitalize): add test for double spaced string

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -4,3 +4,4 @@ test("capitalize a to A", () => expect(capitalize("a")).toBe("A"))
 test("capitalize ab to Ab", () => expect(capitalize("ab")).toBe("Ab"))
 test("spaced: capitalize a b to A B", () =>
   expect(capitalize("a b")).toBe("A B"))
+test("capitalize a  b to A  B", () => expect(capitalize("a  b")).toBe("A  B"))
