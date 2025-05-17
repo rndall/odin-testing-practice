8be84ce526c7ff8fc8b855091ec3b89f514f5456
Ran <96964503+rndall@users.noreply.github.com>
2025-05-17 23:20:36 +0800

    test(capitalize): add second test

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,3 +1,4 @@
 const capitalize = require("./capitalize")
 
 test("capitalize a to A", () => expect(capitalize("a")).toBe("A"))
+test("capitalize ab to Ab", () => expect(capitalize("ab")).toBe("Ab"))
