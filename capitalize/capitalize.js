7a83d7cd00f311f6615086708aa7d4fb6b23b3f5
Ran <96964503+rndall@users.noreply.github.com>
2025-05-17 23:42:57 +0800

    refactor(capitalize): handle multiple words

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,6 +1,7 @@
 function capitalize(string) {
-  if (string === "ab") return "Ab"
-  return "A"
+  const words = string.split(" ")
+  const upper = words.map((word) => word[0].toUpperCase() + word.slice(1))
+  return upper.join(" ")
 }
 
 module.exports = capitalize
