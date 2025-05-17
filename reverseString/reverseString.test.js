d33bed04624185b6125bc1d376fd901897c28a27
Ran <96964503+rndall@users.noreply.github.com>
2025-05-18 00:09:17 +0800

    test(reverseString): add third test

--- reverseString/reverseString.test.js
+++ reverseString/reverseString.test.js
@@ -2,3 +2,4 @@ const reverseString = require("./reverseString")
 
 test("reverse ab to ba", () => expect(reverseString("ab")).toBe("ba"))
 test("reverse abc to cba", () => expect(reverseString("abc")).toBe("cba"))
+test("reverse abcd to dcba", () => expect(reverseString("abcd")).toBe("dcba"))
