0cc9101cf2f870e339055e48417dcc5abcdb46ae
Ran <96964503+rndall@users.noreply.github.com>
2025-05-17 23:58:39 +0800

    test(capitalize): add test for numbered and double spaced string

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -7,3 +7,5 @@ test("spaced: capitalize a b to A B", () =>
 test("capitalize a  b to A  B", () => expect(capitalize("a  b")).toBe("A  B"))
 test("number: capitalize 123 to 123", () =>
   expect(capitalize("123")).toBe("123"))
+test("capitalize 23a  x Ar3 to 23a  X Ar3", () =>
+  expect(capitalize("23a  x Ar3")).toBe("23a  X Ar3"))
