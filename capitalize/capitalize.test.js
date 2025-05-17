6bd2a997f3d841a37619d17dba603fee2a2f8448
Ran <96964503+rndall@users.noreply.github.com>
2025-05-17 23:32:02 +0800

    test(capitalize): add test argument with space

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -2,3 +2,5 @@ const capitalize = require("./capitalize")
 
 test("capitalize a to A", () => expect(capitalize("a")).toBe("A"))
 test("capitalize ab to Ab", () => expect(capitalize("ab")).toBe("Ab"))
+test("spaced: capitalize a b to A B", () =>
+  expect(capitalize("a b")).toBe("A B"))
