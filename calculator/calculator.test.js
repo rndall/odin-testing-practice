3f7dd4c941ff0c6781254cb0982c542b6f37e4aa
Ran <96964503+rndall@users.noreply.github.com>
2025-05-18 00:32:02 +0800

    test(calculator): add test if add function is defined

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -2,4 +2,8 @@ describe("calculator", () => {
   test("is defined", () => {
     expect(require("./calculator")).toBeDefined()
   })
+
+  const calculator = require("./calculator")
+
+  test("add function is defined", () => expect(calculator.add).toBeDefined())
 })
