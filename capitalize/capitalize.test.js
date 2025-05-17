fdf8d7afe4647c315b7f6a0dc01426e8a8505bbc
Ran <96964503+rndall@users.noreply.github.com>
2025-05-17 23:57:01 +0800

    test(capitalize): add test for numbers

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -5,3 +5,5 @@ test("capitalize ab to Ab", () => expect(capitalize("ab")).toBe("Ab"))
 test("spaced: capitalize a b to A B", () =>
   expect(capitalize("a b")).toBe("A B"))
 test("capitalize a  b to A  B", () => expect(capitalize("a  b")).toBe("A  B"))
+test("number: capitalize 123 to 123", () =>
+  expect(capitalize("123")).toBe("123"))
