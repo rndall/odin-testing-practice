cb7fe3c8e084733a1ce967476220e36b3a584f39
Ran <96964503+rndall@users.noreply.github.com>
2025-05-18 22:51:17 +0800

    feat(caesarCipher): handle single char strings

--- caesarCipher/caesarCipher.js
+++ caesarCipher/caesarCipher.js
@@ -1,3 +1,8 @@
-const caesarCipher = () => "c"
+const caesarCipher = (string, shift) => {
+  const code = string.charCodeAt()
+  const shiftedCode = code + shift
+  const cipheredString = String.fromCharCode(shiftedCode)
+  return cipheredString
+}
 
 module.exports = caesarCipher
