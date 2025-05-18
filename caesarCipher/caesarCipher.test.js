a754db25a2dc1aee6bf24c90509c91f7eb040c72
Ran <96964503+rndall@users.noreply.github.com>
2025-05-18 22:52:34 +0800

    test(caesarCipher): add single char string test

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -4,3 +4,5 @@ test("cipher 'a' with 2 shift factor to 'c'", () =>
   expect(caesarCipher("a", 2)).toBe("c"))
 test("cipher 'a' with 3 shift factor to 'd'", () =>
   expect(caesarCipher("a", 3)).toBe("d"))
+test("cipher 'ab' with 2 shift factor to 'cd'", () =>
+  expect(caesarCipher("ab", 2)).toBe("cd"))
