5da1b8a83c2e0c572a3822cbb92f5dbcc7869933
Ran <96964503+rndall@users.noreply.github.com>
2025-05-18 22:44:28 +0800

    test(caesarCipher): add second test

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -2,3 +2,5 @@ const caesarCipher = require("./caesarCipher")
 
 test("cipher 'a' with 2 shift factor to 'c'", () =>
   expect(caesarCipher("a", 2)).toBe("c"))
+test("cipher 'a' with 3 shift factor to 'd'", () =>
+  expect(caesarCipher("a", 3)).toBe("d"))
