b13b72ca1d3ad73dd1c2031d7ee34e4249add21f
Ran <96964503+rndall@users.noreply.github.com>
2025-05-18 22:24:00 +0800

    test(calculator): add second multiply test

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -25,4 +25,6 @@ describe("calculator", () => {
     expect(calculator.subtract(3, 5)).toBe(-2))
 
   test("two times two is four", () => expect(calculator.multiply(2, 2)).toBe(4))
+  test("three times two is six", () =>
+    expect(calculator.multiply(3, 2)).toBe(6))
 })
