6ca050eb8db772f9868bcfeca67b7388a7e35560
Ran <96964503+rndall@users.noreply.github.com>
2025-05-19 18:16:44 +0800

    feat(caesarCipher): implement isAlpha into caesarCipher function

--- caesarCipher/caesarCipher.js
+++ caesarCipher/caesarCipher.js
@@ -4,6 +4,11 @@ const caesarCipher = (string, shift) => {
   let cipheredString = ""
 
   for (const char of string) {
+    if (!isAlpha(char)) {
+      cipheredString += char
+      continue
+    }
+
     const code = char.charCodeAt()
     let shiftedCode = code + shift
     if (shiftedCode >= 123) shiftedCode -= 26
