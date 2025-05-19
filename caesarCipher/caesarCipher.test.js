cbc38b411c94d3ff9be718aeeee0f0a54a245754
Ran <96964503+rndall@users.noreply.github.com>
2025-05-19 18:09:58 +0800

    test(caesarCipher): add test for punctuations

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -10,3 +10,5 @@ test("cipher 'xyz' with 3 shift factor to 'abc'", () =>
   expect(caesarCipher("xyz", 3)).toBe("abc"))
 test("cipher 'HeLLo' with 3 shift factor to 'KhOOr'", () =>
   expect(caesarCipher("HeLLo", 3)).toBe("KhOOr"))
+test("cipher 'Hello, World!' with 3 shift factor to 'Khoor, Zruog!'", () =>
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!"))
