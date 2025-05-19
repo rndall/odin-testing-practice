5598d35e7df9d472ba892a037f518e95333896e4
Ran <96964503+rndall@users.noreply.github.com>
2025-05-19 22:59:42 +0800

    feat(analyzeArray): add max value to output

--- analyzeArray/analyzeArray.js
+++ analyzeArray/analyzeArray.js
@@ -1,8 +1,9 @@
 const analyzeArray = (arr) => {
   const average = arr.reduce((prev, curr) => (prev += curr)) / arr.length
   const min = Math.min(...arr)
+  const max = Math.max(...arr)
 
-  return { average, min }
+  return { average, min, max }
 }
 
 module.exports = analyzeArray
