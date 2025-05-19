69c3332ab91a90c3eb17edafac1d21ee9fdf881a
Ran <96964503+rndall@users.noreply.github.com>
2025-05-19 16:39:18 +0800

    test(caesarCipher): add test for wrapping

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -6,3 +6,5 @@ test("cipher 'a' with 3 shift factor to 'd'", () =>
   expect(caesarCipher("a", 3)).toBe("d"))
 test("cipher 'ab' with 2 shift factor to 'cd'", () =>
   expect(caesarCipher("ab", 2)).toBe("cd"))
+test("cipher 'xyz' with 3 shift factor to 'abc'", () =>
+  expect(caesarCipher("xyz", 3)).toBe("abc"))
