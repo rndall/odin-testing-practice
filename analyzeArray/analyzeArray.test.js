54ad258498c510fe24f032557ab70e2b188cb1ab
Ran <96964503+rndall@users.noreply.github.com>
2025-05-19 22:57:09 +0800

    test(analyzeArray): modify first test case for additional min property

--- analyzeArray/analyzeArray.test.js
+++ analyzeArray/analyzeArray.test.js
@@ -3,14 +3,16 @@ const analyzeArray = require("./analyzeArray")
 describe("analyzeArray", () => {
   test("return average, min, max, and length of given array", () => {
     let array = [1, 8, 3, 4, 2, 6]
+    let result = analyzeArray(array)
 
-    expect(analyzeArray(array)).toEqual({
-      average: 4,
-    })
+    expect(result.average).toBe(4)
+
+    const { average, ...withoutAverage } = result
+    expect(withoutAverage).toEqual({ min: 1 })
 
     array = [5, 5, 9, 7, 1, 2]
 
-    const result = analyzeArray(array)
+    result = analyzeArray(array)
     expect(result.average).toBeCloseTo(4.8333)
   })
 })
