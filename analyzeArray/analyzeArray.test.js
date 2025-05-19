0081530ee260bf3ce5e0d820d6896c5578d2e50d
Ran <96964503+rndall@users.noreply.github.com>
2025-05-19 22:59:00 +0800

    test(analyzeArray): add max property

--- analyzeArray/analyzeArray.test.js
+++ analyzeArray/analyzeArray.test.js
@@ -8,7 +8,7 @@ describe("analyzeArray", () => {
     expect(result.average).toBe(4)
 
     const { average, ...withoutAverage } = result
-    expect(withoutAverage).toEqual({ min: 1 })
+    expect(withoutAverage).toEqual({ min: 1, max: 8 })
 
     array = [5, 5, 9, 7, 1, 2]
 
